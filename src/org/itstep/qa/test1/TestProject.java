5ad5c28de464f45aa48ec218fd5b73de29db8a76
qa <qa.@ru>
2018-08-18 20:29:12 +0300

    new

---
+++ src/org/itstep/qa/test1/TestProject.java
@@ -0,0 +1,8 @@
+package org.itstep.qa.test1;
+
+public class TestProject {
+    public static void main(String[] args) {  // создание класса main, чтобы прога понимала где точка входа
+
+        System.out.println("Получилось");
+    }
+}
